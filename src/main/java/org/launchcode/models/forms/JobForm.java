efd7ace175a2ecdc5c93a7c664578c56e7b4e5b2
Lawrence Lockhart <lawrence.lockhart@fedex.com>
2019-07-19 15:24:54 -0500

    added the 3 other collections for jobData

--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -43,12 +43,11 @@ public class JobForm {
     public JobForm() {
 
         JobData jobData = JobData.getInstance();
-
-        /*
-            TODO #4 - populate the other ArrayList collections needed in the view
-        */
-
+        
         employers = jobData.getEmployers().findAll();
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
 
     }
 
